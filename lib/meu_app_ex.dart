e15bbc2fa0973ef7ec39c2ca7099499a16fd79a7
Edson Barros <edsonabarros01@gmail.com>
2022-10-26 12:11:23 -0300

    Finalização do projeto para exception

--- lib/meu_app_ex.dart
+++ lib/meu_app_ex.dart
@@ -1,22 +1,41 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:meu_app_ex/exception/nome_invalido_exception.dart';
 import 'package:meu_app_ex/models/Aluno.dart';
 import 'package:meu_app_ex/models/ConsoleUtils.dart';
 
 void execute() {
   print("Bem-vindo ao sistema de notas! ");
-  print("Digite o nome do aluno: ");
-  String nome = ConsoleUtils.lerStringComtext("Digite o nome do aluno: ");
-  var aluno = Aluno(nome);
+  String nome = ConsoleUtils.lerStringComTexto("Digite o nome do aluno: ");
 
-/* 
   try {
-    double numero = double.parse(line ?? "");
-    print(line);
+    if (nome.trim() == "") {
+      throw NomeInvalidoException();
+    }
+  } catch (NomeInvalidoException) {
+    print(NomeInvalidoException);
+    exit(0);
   } catch (e) {
-    print("Número inválido: $line");
-  } finally {
-    print("Executando finally");
-  } */
+    print(e);
+    exit(0);
+  }
+
+  var aluno = Aluno(nome);
+  double? nota;
+  do {
+    nota = ConsoleUtils.lerDoubleComTextoComSaida(
+        "Digite a nota ou S para sair", "S");
+
+    if (nota != null) {
+      aluno.adicionarNota(nota);
+    }
+  } while (nota != null);
+  print("As notas digitadas foram: ${aluno.getNotas()}");
+  print("A média das notas foi: ${aluno.retornaMedia()}");
+  if (aluno.aprovado(7)) {
+    print("O aluno ${aluno.getNome()} foi aprovado");
+  } else {
+    print("O aluno ${aluno.getNome()} foi reprovado");
+  }
 }
--- lib/models/ConsoleUtils.dart
+++ lib/models/ConsoleUtils.dart
@@ -6,8 +6,37 @@ class ConsoleUtils {
     return stdin.readLineSync(encoding: utf8) ?? "";
   }
 
-  static String lerStringComtext(String texto) {
-    lerString();
+  static String lerStringComTexto(String texto) {
+    print(texto);
     return lerString();
+  }
+
+  static double? lerDouble() {
+    var value = lerString();
+    try {
+      return double.parse(value);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static double? lerDoubleComTexto(String texto) {
+    print(texto);
+    return lerDouble();
+  }
+
+  static double? lerDoubleComTextoComSaida(String texto, String valorSaida) {
+    do {
+      try {
+        var value = lerStringComTexto(texto);
+
+        if (value == valorSaida) {
+          return null;
+        }
+        return double.parse(value);
+      } catch (e) {
+        print(texto);
+      }
+    } while (true);
   }
 }
